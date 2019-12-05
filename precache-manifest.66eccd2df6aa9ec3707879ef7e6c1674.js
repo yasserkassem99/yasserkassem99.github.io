30b234524c940cdf78c5e1056b2d433ef0ead688
yasserkassem99 <yasser.kassem@minagate.com>
2019-12-05 16:16:53 +0200

    Initial commit

---
+++ precache-manifest.66eccd2df6aa9ec3707879ef7e6c1674.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be3954683dc57b9d51a662387463644f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "062907ae08a1a22f2c31",
+    "url": "/static/css/10.3efa3c1c.chunk.css"
+  },
+  {
+    "revision": "afc3e605b00c65535424",
+    "url": "/static/css/11.1150ca12.chunk.css"
+  },
+  {
+    "revision": "b09faf3bc8ed0b61bfe6",
+    "url": "/static/css/12.31d0d7c5.chunk.css"
+  },
+  {
+    "revision": "d0ea0e0be8b309e9d22f",
+    "url": "/static/css/13.fef83134.chunk.css"
+  },
+  {
+    "revision": "f2755ff49ed7c1bf6886",
+    "url": "/static/css/14.4b0686b9.chunk.css"
+  },
+  {
+    "revision": "fd9095f081311211af77",
+    "url": "/static/css/15.f2d1d30e.chunk.css"
+  },
+  {
+    "revision": "2ff8953c4b498215f146",
+    "url": "/static/css/8.945747aa.chunk.css"
+  },
+  {
+    "revision": "2fe611cc10b432187a5b",
+    "url": "/static/css/9.33bd88f7.chunk.css"
+  },
+  {
+    "revision": "6bd4ca3dc3656bbda365",
+    "url": "/static/css/main.6e6df2c6.chunk.css"
+  },
+  {
+    "revision": "53d82082ca96ffdf6234",
+    "url": "/static/js/0.8c3ede10.chunk.js"
+  },
+  {
+    "revision": "402be2c96592dd8ea360",
+    "url": "/static/js/1.2bed4b90.chunk.js"
+  },
+  {
+    "revision": "062907ae08a1a22f2c31",
+    "url": "/static/js/10.33db25d0.chunk.js"
+  },
+  {
+    "revision": "afc3e605b00c65535424",
+    "url": "/static/js/11.4e75bc4a.chunk.js"
+  },
+  {
+    "revision": "b09faf3bc8ed0b61bfe6",
+    "url": "/static/js/12.eba76d4f.chunk.js"
+  },
+  {
+    "revision": "d0ea0e0be8b309e9d22f",
+    "url": "/static/js/13.b0530713.chunk.js"
+  },
+  {
+    "revision": "f2755ff49ed7c1bf6886",
+    "url": "/static/js/14.624a4981.chunk.js"
+  },
+  {
+    "revision": "fd9095f081311211af77",
+    "url": "/static/js/15.62608fd2.chunk.js"
+  },
+  {
+    "revision": "4a49c28250e518bc17c3",
+    "url": "/static/js/2.2f6db19f.chunk.js"
+  },
+  {
+    "revision": "a704587501c0a235e4bc",
+    "url": "/static/js/3.696a17f7.chunk.js"
+  },
+  {
+    "revision": "5eefe2b358f3997d59c5",
+    "url": "/static/js/4.1431f71f.chunk.js"
+  },
+  {
+    "revision": "db08d237349156e470b7",
+    "url": "/static/js/7.d5c8c2d0.chunk.js"
+  },
+  {
+    "revision": "2ff8953c4b498215f146",
+    "url": "/static/js/8.5494c1cd.chunk.js"
+  },
+  {
+    "revision": "2fe611cc10b432187a5b",
+    "url": "/static/js/9.fc34ea28.chunk.js"
+  },
+  {
+    "revision": "6bd4ca3dc3656bbda365",
+    "url": "/static/js/main.34dfc0a2.chunk.js"
+  },
+  {
+    "revision": "4fedc24f93c1ff5b1449",
+    "url": "/static/js/runtime-main.9ae6d602.js"
+  },
+  {
+    "revision": "33d78ddccafc367906b276a80f7d22d4",
+    "url": "/static/media/car-makes-icons.33d78ddc.svg"
+  },
+  {
+    "revision": "589b01e58ba61b9b87ee1891aeb0ef34",
+    "url": "/static/media/car-makes-icons.589b01e5.woff2"
+  },
+  {
+    "revision": "8abe3afb13d3532d66bfbbe1bd50777f",
+    "url": "/static/media/car-makes-icons.8abe3afb.ttf"
+  },
+  {
+    "revision": "a91bd4fd97bf01654eee4781d1670aa4",
+    "url": "/static/media/car-makes-icons.a91bd4fd.eot"
+  },
+  {
+    "revision": "b072f6491c6fa3509b6aeec4bedf96dc",
+    "url": "/static/media/car-makes-icons.b072f649.woff"
+  },
+  {
+    "revision": "6524b3a534bd405f1004dd3cb77c3299",
+    "url": "/static/media/idCard.6524b3a5.png"
+  },
+  {
+    "revision": "df803060910f2e04e3ffd98b2d0a6920",
+    "url": "/static/media/preview.df803060.png"
+  }
+]);
